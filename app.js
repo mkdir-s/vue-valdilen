f3862a49c2456de576c7dee82503ee5a655002bc
Alexeyenko Artyom <i.html06@gmail.com>
2022-07-21 11:04:24 +0600

    feat: realize todoList

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const App = {
     data() {
         return {
-            counter: 0,
+            placeholderString: 'введите название заметки..',
+            title: 'Список задач',
+            inputValue: '',
+            notes: ['Заметка 1'],
+        }
+    },
+    methods: {
+        addHandler() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue);
+                this.inputValue = '';
+            }
+        },
+        removeNote(idx) {
+            this.notes.splice(idx, 1);
+        },
+        toUpperCase(item) {
+            return item.toUpperCase();
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2;
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if(value.length > 50) {
+                this.inputValue = '';
+            }
         }
     }
 }
